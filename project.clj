8ddc7d51c48771bf3d8fa4c707d2f041d33fbace
Philipp Meier <philipp@meier.name>
2009-12-17 00:00:28 +0100

    Leinify

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject org.clojars.ordnungswidrig/clj-conneg "0.1.1-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
+  :dev-dependencies [[org.clojure/swank-clojure "1.0"]
+		     [lein-clojars "0.5.0-SNAPSHOT"]])
